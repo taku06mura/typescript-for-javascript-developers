e3b49cc9fae105ba55a9af3cbddf5451b85ad636
taku06mura <t.lux.g1106@icloud.com>
2020-06-30 17:07:16 +0900

    update to jenerics

--- src/jenerics.ts
+++ src/jenerics.ts
@@ -6,19 +6,22 @@ export {};
 //   return arg;
 // };
 
-// // 方が異なるだけでやっていることが同じような関数を共通化したい。そんな時jenericsが使える。
-
 // const echo = (arg: string): string => {
 //   return arg;
 // }
 
-const echo = <T>(arg: T): T => { // <T>がjenericsの宣言。抽象的な型。慣習としてTがよく使われる。型引数と呼ばれる。この時点では何の型かはわからないが、引数の型と戻り値の型が一致している。
+// 上記のような、型が異なるだけでやっていることが同じような関数を共通化したい。そんな時jenericsが使える。
+
+const echo = <T>(arg: T): T => { // <T>がjenericsの宣言。抽象的な型。慣習としてTがよく使われる。型引数と呼ばれる。この時点では何の型かはわからないが、引数の型と戻り値の型は一致している。
   return arg;
 }
 
 console.log(echo<number>(100)); //ここで何の型か教えてあげる。
 console.log(echo<string>('Hello!')); // このように抽象的な関数をjenericsで作ってしまえば、型を指定するだけで
 console.log(echo<boolean>(true)); // いくらでも関数を作れてしまうので、関数を作るコストが下げられる。
+
+
+// クラスの場合
 
 // class Mirror {
 //   constructor(public value: number) {} // number型で初期化。
